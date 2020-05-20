589c5e2a4c0f272cc50244951e60577704b8203f
ShabdVeyyakula <47516044+ShabdVeyyakula@users.noreply.github.com>
2020-05-20 10:44:45 -0700

    fff

--- Js/dynamicContent.js
+++ Js/dynamicContent.js
@@ -10,6 +10,39 @@ var firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-let output = "";
-formattedUser = localStorage.getItem("email");
+function getHackathonData() {
+    var emailRef = localStorage.get("email")
+var hackathonsRef = firebase.database().ref().child("UserData").child(emailRef).child("Hackathons");
 
+hackathonsRef.once("value", (snap) => {
+    var hackathonsList = [];
+
+    console.log(snap.val());
+    rawData = snap.val();
+
+    if(rawData !== null){
+        snap.forEach((child) => {
+            hackathonsList.push([child.child("Code").val(), child.child("Date").val(), child.child("Hackathon Name").val(), child.child("Role").val()]);
+        });
+    }
+
+    console.log(hackathonsList);
+
+    
+
+}).then(function(){
+
+    
+
+    for(let i = 0; i <= hackathonsList.length; i++){
+        /*
+        elem = document.createElement("tr");
+        elem.innerHTML = '<th scope="col" class="leaderboardData">1</th><th scope="col" class="leaderboardData">Shabd Veyyakul1a</th><th scope="col" class="leaderboardData">20321</th><th scope="col" class="leaderboardData">Level 1</th>';
+        elem.classList.add("leaderboardData")
+        elem.innerHTML = '<th scope="col" class="leaderboardData">'+(i+1).toString()+'</th><th scope="col" class="leaderboardData">' + dataList[i][0] + '</th><th scope="col" class="leaderboardData">' + dataList[i][1] + '</th><th scope="col" class="leaderboardData">Level ' +dataList[i][2] + '</th>';
+        original.appendChild(elem);
+        */
+    };
+});
+
+}
